8b0bf324d3f01100a0186982aad53b9aef6ff817
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-09-16 16:44:48 -0400

    displays messages if the user wins or loses. elements turn green when the user wins, else they turn red

--- numberGuesser/app.js
+++ numberGuesser/app.js
@@ -28,24 +28,64 @@ maxNum.textContent = max;
 // Listen for guess
 guessBtn.addEventListener('click', function(){
     let guess = parseInt(guessInput.value);
-    console.log(guess);
     
     if(isNaN(guess) || guess < min || guess > max){
         setMessage(`Please enter a number between ${min} and ${max}`, 'red');
-    }
-    
-    // Check if won
-    if(guess === winningNum){
-        // Disable input
-        guessInput.disabled = true;
-        // Change border color
-        guessInput.style.borderColor = 'green';
-        // Set message
-        setMessage(`${winningNum} is correct, you win!`, 'green');
     } else {
+    
+        // Check if won
+        if(guess === winningNum){
+            //Game over - won
+            
+            // Set winning message
+            let msg = `${winningNum} is correct, you win!`;
+            gameOver(true, msg);
 
+        } else {
+            // Wrong number
+
+            // Decrease number of guesses
+            guessesLeft -= 1;
+            
+            if(guessesLeft === 0){
+                //Game over - lost
+                let msg = `Game over...you lost. The correct number was ${winningNum}.`;
+                gameOver(false, msg);
+
+            } else {
+                // Game continues - answer wrong
+                
+                // Change border color
+                guessInput.style.borderColor = 'red';
+                
+                // Clear Input
+                guessInput.value = '';
+
+                // Tell user its the wrong number.
+                setMessage(`${guess} is not correct, ${guessesLeft} guesses remaining`, 'red');
+            }
+        }
     }
 });
+
+//Game over
+function gameOver(won, msg){
+    // Disable input
+    guessInput.disabled = true;
+    
+    // Change border color
+    let color;        
+    won ? color = 'green' : color = 'red'
+    
+    // Set border color
+    guessInput.style.borderColor = color;
+    
+    // Set input text color
+    guessInput.style.color = color;
+    
+    // Set message
+    setMessage(msg, color);
+}
 
 function setMessage(msg, color){
     message.style.color = color;
